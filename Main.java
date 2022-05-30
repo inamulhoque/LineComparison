4c252917912f940fdbdf68671b8121855598250f
Inamul <inamulhoque097@gmail.com>
2022-05-30 23:54:41 +0530

    Added: Calculating length of line

--- Main.java
+++ Main.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
 public class Main {
-    public static void calculation(){
-        System.out.println("Enter the co-ordinates:(x1,y1) & (x2,y2) ");
-        Scanner myObj = new Scanner(System.in);
-        System.out.print("x1: ");
-        int x1 = myObj.nextInt();
-        System.out.print("y1: ");
-        int y1 = myObj.nextInt();
-        System.out.print("x2: ");
-        int x2 = myObj.nextInt();
-        System.out.print("y2: ");
-        int y2 = myObj.nextInt();
-
+    public static void calculation(double x1, double y1, double x2, double y2){
         double lenOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
-        System.out.print("Length of line is: "+lenOfLine);
+        System.out.println("Length of line: "+lenOfLine);
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison computation program on master branch.");
-        Main.calculation();
+        double x1 = Math.floor(Math.random()*10);
+        double y1 = Math.floor(Math.random()*10);
+        double x2 = Math.floor(Math.random()*10);
+        double y2 = Math.floor(Math.random()*10);
+        Main.calculation(x1, y1, x2, y2);
     }
 }
